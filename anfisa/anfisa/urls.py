019c2e8bf481e0579534ca583f5ad623daa77d5c
Денис Поляков <74591884+msdrakula@users.noreply.github.com>
2023-02-17 15:45:02 +0300

    urls

--- anfisa/anfisa/urls.py
+++ anfisa/anfisa/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# anfisa/urls.py (главный файл url проекта)
+# По умолчанию в проект Django подключена система администрирования 
 from django.contrib import admin
-from django.urls import path
+# Функция include позволит использовать path() из других файлов.
+# Импортируем!
+from django.urls import include, path 
 
-urlpatterns = [
+urlpatterns = [    
+    # Дорогой Джанго, если на сервер пришёл любой запрос (''),
+    # перейди в файл urls приложения ice_cream 
+    # и проверь там все path() на совпадение с запрошенным URL
+    path('', include('ice_cream.urls')),
+    # Если в приложении ice_cream не найдётся совпадений -
+    # Django продолжит искать совпадения здесь, в головном файле urls.py.
+
+    # Встроенная админка Django подключена «из коробки» по адресу admin/
     path('admin/', admin.site.urls),
-]
+] 
